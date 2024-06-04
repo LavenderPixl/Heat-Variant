f6fc0bb4778b0996f739f45a0dd9a589b54cb49f
LavenderPixl <annajacobsen1999@hotmail.com>
2024-06-04 14:53:28 +0200

    Classes/models created for DB.
    Working on uploading data to DB.

--- Backend/app.py
+++ Backend/app.py
@@ -1,58 +1,86 @@
-﻿import influxdb_client, os, time
+﻿import datetime
+import http
+import asyncio
+import influxdb_client, os, time
+import uvicorn
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 load_dotenv()
 app = FastAPI()
 
 token = os.getenv("INFLUXDB_TOKEN")
 org = "heat_variant"
-bucket = "variants"
+bucket = "Air Data"
 url = "http://10.0.0.2:8086"
 
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 
-@app.get("/test")
-async def test():
-    return {"Hello World": "Testing, again.!"}
+class Apartment(BaseModel):
+    apt_id: int
+    mc_id: str | None = None
+    floor: int
+    apt_number: str
+    email: str
+    phone_number: str
 
 
-@app.get("/get-air-data")
-async def get_air_data(apt_number: str):
-    return {f""}
+class AirData(BaseModel):
+    mc_id: str
+    temperature: float = 0
+    pressure: float = 0
+    humidity: float = 0
+    air_quality: float = 0
 
 
-@app.get("/get-temperature")
-async def get_temperature(temp: str):
-    return {f"temperature": temp}
+class Billing(BaseModel):
+    apt_id: int
+    billing_id: int
+    amount: float = 0.00
+    date_issued: str | None = None
+    date_paid: str | None = None
+    paid: bool
 
 
-@app.get("/get-pressure")
-async def get_pressure(pressure: str):
-    return {f"pressure": pressure}
+@app.post("/apt_info")
+async def test(apartment: Apartment):
+    try:
+        print(f"Method used. Apartment: {apartment}")
+        return {"apartment": apartment.dict()}
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
-@app.get("/get-humidity")
-async def get_humidity(humidity: str):
-    return {f"humidity": humidity}
+@app.post("/get-air-data")
+async def get_temperature(data: AirData):
+    print(f"Received temperature: {data.temperature}, Pressure: {data.pressure}, humidity: {data.humidity}, "
+          f"air quality: {data.air_quality}, mc_id: {data.mc_id}")
+    await deliver_data(data)
+    return {"mc_id": data.mc_id, "temperature": data.temperature, "pressure": data.pressure, "humidity": data.humidity,
+            "air_quality": data.air_quality}
 
 
-@app.get("/get-gas")
-async def get_gas(gas: str):
-    return {f"gas": gas}
+async def deliver_data(data: AirData):
+    p = (
+        Point("Air")
+        .tag("data.apt_id", 1)
+        .field("temperature", data.temperature)
+        .field("pressure", data.pressure)
+        .field("humidity", data.humidity)
+        .field("air_quality", data.air_quality)
+    )
+    write_api.write(bucket, org, p)
+    time.sleep(1)
 
 
-# Temperature = 27.08 *C
-# Pressure = 1004.68 hPa
-# Humidity = 49.33 %
-# Gas = 148.23 KOhms
-# Approx. Altitude = 71.60 m
-
-print("Connected.")
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
 
 # for value in range(5):
 #     point = (
