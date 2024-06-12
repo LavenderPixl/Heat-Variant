a6800215473e39d8c59e7d8a10b359a2bf15d2bb
LavenderPixl <annajacobsen1999@hotmail.com>
2024-06-12 11:52:46 +0200

    Switching to class B network (172.0.0.0) instead of A (10.0.0.0)
    Regions created (MySQL, InfluxDB, Classes)
    
    insert_seed_apartments() method created.
    - Inserts seed data into apartments table, in the Heat Variant MySQL database.
    
    reset_apartment_table() method created.
    - Drops the apartment table and re-creates it.

--- Backend/app.py
+++ Backend/app.py
@@ -22,26 +22,26 @@ msdb = mysql.connector.connect(
 ms = msdb.cursor()
 try:
     ms.execute("CREATE DATABASE IF NOT EXISTS HeatVariant")
+    print("DB HeatVariant created successfully.")
     ms.execute("USE HeatVariant")
-    ms.execute("CREATE TABLE IF NOT EXISTS Apartments (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(32))")
-    print("Db HeatVariant created successfully.")
-
-    for x in ms:
-        print(x)
+    ms.execute("CREATE TABLE IF NOT EXISTS Apartments (id INT PRIMARY KEY AUTO_INCREMENT, mc_id VARCHAR(32), "
+               "floor INT, apt_number VARCHAR(32), email VARCHAR(32), phone_number VARCHAR(32))")
+    print("Apartment table created successfully.")
 except mysql.connector.Error as err:
     print("Something went wrong: {}".format(err))
 
 token = os.getenv("INFLUXDB_TOKEN")
 org = "heat_variant"
 bucket = "Air Data"
-influx_url = "http://10.0.0.2:8086"
+influx_url = "http://172.0.0.2:8086"
 
 client = influxdb_client.InfluxDBClient(url=influx_url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 
+# region Classes
 class Apartment(BaseModel):
-    apt_id: int
+    id: int
     mc_id: str | None = None
     floor: int
     apt_number: str
@@ -66,23 +66,17 @@ class Billing(BaseModel):
     paid: bool
 
 
-@app.post("/apt_info")
-async def test(apartment: Apartment):
-    try:
-        print(f"Method used. Apartment: {apartment}")
-        return {"apartment": apartment.dict()}
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+# endregion
+
+# region InfluxDB
 
 
 @app.post("/get-air-data")
 async def get_temperature(data: AirData):
-    print(f"Received temperature: {data.temperature}, Pressure: {data.pressure}, humidity: {data.humidity}, "
-          f"air quality: {data.air_quality}, mc_id: {data.mc_id}")
+    print(f"Received temperature: {data.temperature}, Pressure: {data.pressure}, Humidity: {data.humidity}, "
+          f"Air Quality/Gas: {data.air_quality}, Mc_ID: {data.mc_id}")
     await deliver_data(data)
-    return {"mc_id": data.mc_id, "temperature": data.temperature, "pressure": data.pressure, "humidity": data.humidity,
-            "air_quality": data.air_quality}
+    return True
 
 
 async def deliver_data(data: AirData):
@@ -100,6 +94,50 @@ async def deliver_data(data: AirData):
         print(f"Bucket cannot be found. Error: {e}")
     time.sleep(1)
 
+
+# endregion
+
+# region MySQL
+@app.post("/insert_new_apartment")
+@app.post("/insert_seed_apartments")
+async def insert_seed_apartments():
+    try:
+        ms.execute("USE HeatVariant")
+        sql = "INSERT INTO Apartments (mc_id, floor, apt_number, email, phone_number) VALUES (%s, %s, %s, %s, %s)"
+        val = [
+            ("08:3A:F2:A8:C5:9C", 1, 2, "email@email.com", "22548032"),
+            ("NULL", 1, 3, "email2@email.com", "34194673")
+        ]
+        ms.executemany(sql, val)
+        msdb.commit()
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+
+
+@app.post("/reset_apartment_table")
+async def reset_apartment_table():
+    try:
+        ms.execute("USE HeatVariant")
+        sql = "DROP TABLE Apartments"
+        ms.execute(sql)
+        ms.execute("CREATE TABLE Apartments (id INT PRIMARY KEY AUTO_INCREMENT, mc_id VARCHAR(32), "
+                   "floor INT, apt_number VARCHAR(32), email VARCHAR(32), phone_number VARCHAR(32))")
+        msdb.commit()
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        
+
+@app.post("/apt_info")
+async def test(apartment: Apartment):
+    try:
+        print(f"Method used. Apartment: {apartment}")
+        return {"apartment": apartment.dict()}
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+# endregion
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
